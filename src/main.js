01a3da429fdd9af5f1589f698da8b4d23306924a
Ramazan <r777wgx@gmail.com>
2023-04-03 20:37:44 +0400

    setup complete

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "@/router";
+import vue3GoogleLogin from "vue3-google-login";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+app.use(vue3GoogleLogin, {
+  clientId: "",
+});
+
+app.mount("#app");
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class=" text-4xl  bg-orange-500 py-2">Home Page</h1>
+    </div>
+</template>
