16043975a9455c81a83fb6c41303e7b15faf152b
Ramazan <r777wgx@gmail.com>
2023-04-04 20:51:35 +0400

    router setup complete

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from '@/views/HomeView.vue';
+import LoginView from '@/views/LoginView.vue';
 
 const routes = [
   {
     path: "/",
     component: HomeView,
+  },
+  {
+    path: "/login",
+    component: LoginView,
   }
 ];
 
