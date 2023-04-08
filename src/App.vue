2e5b56c9078dd7be76dd0a3b1b2c6da849183158
Ramazan <frist.r5vzn@gmail.com>
2023-04-08 20:00:52 +0400

    middleware

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,15 @@
   <router-view />
 </template>
 
-<script>
-</script>
+<script setup>
+  import { onMounted } from 'vue';
+  import { useUserStore } from '@/store/user-store.js';
+  import { useRouter } from 'vue-router';
+  
+  let userStore = useUserStore();
+  const router = useRouter();
 
-<style>
-</style>
+  onMounted(() => {
+    if(!userStore.sub) return router.push('/login');
+  })
+</script>
